a7cfc3c06d4417b432a413fd8bb8c3e08b66aba8
sijoonlee <shijoonlee@gmail.com>
2019-04-07 21:56:22 -0400

    fix error

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ const path = require('path')
 const express = require('express')
 const bcrypt = require('bcryptjs')
 
-// /home/sijoonlee/mongodb4.0.6/bin/mongod --dbpath=/home/sijoonlee/Documents/mongodb_data
 const publicDirectoryPath = path.join(__dirname, '../public')
-// const viewsPath = path.join(__dirname, '../templates/views')
-// const partialsPath = path.join(__dirname, '../templates/partials')
-
-// Setup handlebars engine and views location
-
 
 require('./db/mongoose')
 const memoRouter = require('./router/memo')
@@ -17,7 +11,6 @@ const memoRouter = require('./router/memo')
 const app = express()
 app.use(express.static(publicDirectoryPath))
 const port = process.env.PORT
-//https://www.npmjs.com/package/env-cmd
 
 app.use(express.json())
 //app.use(userRouter)
